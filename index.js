b9ed52c61aa505cc83c173fbdd7f86bb2053590a
Michael Onafowokan <bolutolu12@gmail.com>
2025-02-18 23:00:22 +0100

    Moving to javascript for functionality

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const mobileNavBtn = document.getElementById("mobileNavBtn");
+const radioInputs = document.querySelectorAll("input[type='radio']");
+const form = document.getElementById("form");
+const closeModal = document.querySelector(".close-modal");
+const projectModal = document.querySelector(".project-modal");
 
 mobileNavBtn.addEventListener("click", (e) => {
     const navWrapper = mobileNavBtn.parentElement;
     navWrapper.classList.toggle("open")
 
 });
+
+radioInputs.forEach((radio) => {
+    
+    radio.addEventListener("change", (e) => {
+        radioInputs.forEach((radio) => {
+            radio.closest("label").classList.remove("selected-card");
+           
+        });
+
+        const label = e.target.closest("label");
+        const bottom = label.querySelector(".bottom");
+
+        label.classList.add("selected-card");
+        bottom.style.height = `${bottom.scrollHeight}px`;
+        
+     
+       
+    });
+    
+});
+
+closeModal.addEventListener("click", () => {
+    document.body.classList.remove("project-modal-open");
+})
 
 
